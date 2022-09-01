71e5a0069498cdda39ff28b42364a24fde9b72cd
Ezen <82026690+ezengaston@users.noreply.github.com>
2022-09-01 17:09:02 +0300

    Tabs added

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,24 @@
 <script>
   import Header from "./components/Header.svelte";
   import Footer from "./components/Footer.svelte";
+  import Tabs from "./shared/Tabs.svelte";
+
+  let items = ["Current Polls", "Add New Poll"];
+  let activeItem = "Current Polls";
+
+  const tabChange = (e) => {
+    activeItem = e.detail;
+  };
 </script>
 
 <Header />
 <main>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat architecto
-    doloremque consequuntur dicta recusandae totam, omnis earum aliquid
-    reprehenderit, temporibus in nostrum reiciendis sint fuga vel voluptatibus
-    nam sed tenetur.
-  </p>
+  <Tabs {items} {activeItem} on:tabChange={tabChange} />
+  {#if activeItem === "Current Polls"}
+    <p>Poll list component goes here</p>
+  {:else if activeItem === "Add New Poll"}
+    <p>Add new poll component goes here</p>
+  {/if}
 </main>
 <Footer />
 
