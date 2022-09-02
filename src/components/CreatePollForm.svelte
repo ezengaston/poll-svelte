2c9e32e1a0d1b1724f2dc4dad6025578cb2488e2
Ezen <82026690+ezengaston@users.noreply.github.com>
2022-09-02 14:53:14 +0300

    Validator added

--- src/components/CreatePollForm.svelte
+++ src/components/CreatePollForm.svelte
@@ -6,9 +6,40 @@
     answerA: "",
     answerB: "",
   };
+  let errors = {
+    question: "",
+    answerA: "",
+    answerB: "",
+  };
+  let valid = false;
 
   const submitHandler = () => {
-    console.log(fields);
+    valid = true;
+
+    if (fields.question.trim().length < 5) {
+      valid = false;
+      errors.question = "Question must be at least 5 characters long";
+    } else {
+      errors.question = "";
+    }
+
+    if (fields.answerA.trim().length < 1) {
+      valid = false;
+      errors.answerA = "Answer A cannot be empty";
+    } else {
+      errors.answerA = "";
+    }
+
+    if (fields.answerB.trim().length < 1) {
+      valid = false;
+      errors.answerB = "Answer B cannot be empty";
+    } else {
+      errors.answerB = "";
+    }
+
+    if (valid) {
+      console.log("valid");
+    }
   };
 </script>
 
@@ -16,14 +47,17 @@
   <div class="form-field">
     <label for="question">Poll Question:</label>
     <input type="text" id="question" bind:value={fields.question} />
+    <div class="error">{errors.question}</div>
   </div>
   <div class="form-field">
     <label for="answer-a">Answer A:</label>
     <input type="text" id="answer-a" bind:value={fields.answerA} />
+    <div class="error">{errors.answerA}</div>
   </div>
   <div class="form-field">
     <label for="answer-b">Answer B:</label>
     <input type="text" id="answer-b" bind:value={fields.answerB} />
+    <div class="error">{errors.answerB}</div>
   </div>
   <Button type="secondary" flat={true}>Add Poll</Button>
 </form>
@@ -47,5 +81,11 @@
   label {
     margin: 10px auto;
     text-align: left;
+  }
+
+  .error {
+    font-weight: bold;
+    font-size: 12px;
+    color: #d91b42;
   }
 </style>
