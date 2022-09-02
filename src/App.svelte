ef86f8ade17b00fee3b292102166e3f16b749598
Ezen <82026690+ezengaston@users.noreply.github.com>
2022-09-02 16:46:01 +0300

    PollStore added

--- src/App.svelte
+++ src/App.svelte
@@ -8,17 +8,6 @@
   let items = ["Current Polls", "Add New Poll"];
   let activeItem = "Current Polls";
 
-  let polls = [
-    {
-      id: 1,
-      question: "Python or JavaScript?",
-      answerA: "Python",
-      answerB: "JavaScript",
-      votesA: 9,
-      votesB: 15,
-    },
-  ];
-
   const tabChange = (e) => {
     activeItem = e.detail;
   };
@@ -51,7 +40,7 @@
 <main>
   <Tabs {items} {activeItem} on:tabChange={tabChange} />
   {#if activeItem === "Current Polls"}
-    <PollList {polls} on:vote={handleVote} />
+    <PollList on:vote={handleVote} />
   {:else if activeItem === "Add New Poll"}
     <CreatePollForm on:add={handleAdd} />
   {/if}
